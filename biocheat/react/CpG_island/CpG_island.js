14b3ba54848d3c06d01f2c157a287d58e6d3ca5a
Ryan Rafe Lee <holo@wisewolf.org>
2016-11-10 02:30:23 +0900

    detect CpG island

--- biocheat/react/CpG_island/CpG_island.js
+++ biocheat/react/CpG_island/CpG_island.js
@@ -4,61 +4,154 @@ class CpGIsland extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
-			codon_input:"",
+			base_input: "",
+			base_seq: [],
 			obs_exp_threshold: 0.6,
 			GC_content_threshold: 0.5,
 			scanning_window_size: 200,
 		}
 	}
 
-	codon_textarea_changed(e){
+	base_textarea_changed(e){
+		var base_seq= e.target.value.toUpperCase().replace(/[^ATUCG]/g,"").trim().split("");
+
 		this.setState({
-			codon_input: e.target.value,
-			codon_seq: e.target.value.toUpperCase().replace(/[^ATUCG]/g,"").trim(),
+			base_input: e.target.value,
+			base_seq: base_seq,
 
 		});
 	}
 
 	FASTA_file_changed(e){
 		var reader= new FileReader();
+		var base_seq= e.target.result.replace(/^>.+\n/,"").toUpperCase().replace(/[^ATUCG]/g,"").trim().split("");
+
 		reader.onload= function (e){
 			this.setState({
-				codon_input: e.target.result.replace(/^>.+\n/,""),
-				codon_seq: e.target.result.replace(/^>.+\n/,"").toUpperCase().replace(/[^ATUCG]/g,"").trim(),
+				base_input: e.target.result.replace(/^>.+\n/,""),
+				base_seq: base_seq,
 			})
 
 		}.bind(this);
 		reader.readAsText(e.target.files[0]);
 	}
 
+	detect_CpG_island(){
+		var num_c=0;
+		var num_g=0;
+		var num_cg=0;
+		var scanning_window=[];
+
+		var CpG_islands=[];
+
+		this.state.base_seq.forEach( (base, idx) => {
+			var prev_input= scanning_window.slice(-1);
+			scanning_window.push(base);
+
+			if(base== "C"){
+				num_c++;
+			}else if(base=="G"){
+				num_g++;
+			}
+			if(prev_input=="C" && base=="G"){
+				num_cg++;
+			}
+
+			if(scanning_window.length< this.state.scanning_window_size){
+				return;
+			}
+
+			if(scanning_window.length> this.state.scanning_window_size){
+				var base_to_loss= scanning_window.shift();
+				if(base_to_loss== "C"){
+					num_c--;
+				}
+				if(base_to_loss== "G"){
+					num_g--;
+				}
+				if(base_to_loss=="C" && scanning_window[0]=="G"){
+					num_cg--;
+				}
+			}
+
+			var obs_exp= num_cg/((num_c* num_g)/scanning_window.length);
+			var GC_content= (num_c+ num_g)/scanning_window.length
+
+			if(obs_exp>this.state.obs_exp_threshold && GC_content>this.state.GC_content_threshold){
+				CpG_islands.push([idx, obs_exp, GC_content]);
+			}
+		})
+		return CpG_islands;
+	}
+
+	obs_exp_threshold_changed(e){
+		this.setState({
+			obs_exp_threshold: e.target.value,
+		})
+	}
+
+	gc_content_threshold_changed(e){
+		this.setState({
+			gc_content_threshold: e.target.value,
+		})
+	}
+
+	scanning_window_size_changed(e){
+		this.setState({
+			scanngin_window_size: e.target.value,
+		})
+	}
+
+	render_CpG_islands(CpG_islands){
+		var scanning_window_size= this.state.scanning_window_size;
+		return <div id="CpG_island_list_div" className="collapse in">
+			<ul className="list-unstyled">
+				{CpG_islands.map( (e)=> {
+					return <li>
+						CpG island at <b>{e[0]-scanning_window_size}</b> to <b>{e[0]}</b> (Obs/Exp={e[1]}, GC Content={e[2]})
+					</li>
+				})}
+			</ul>
+		</div>
+	}
+
 	render(){
-			return <div className="col-sm-12">
-				<div className="col-sm-12 form-group">
-					<label className="col-sm-3">Obs/Exp Threshold (0-1)</label>
-					<div className="col-sm-4">
-						<input className="form-control" defaultValue={this.state.obs_exp_threshold} />
-					</div>
-				</div>
-				<div className="col-sm-12 form-group">
-					<label className="col-sm-3">GC Content Threshold (0-1)</label>
-					<div className="col-sm-4">
-						<input className="form-control" defaultValue={this.state.GC_content_threshold} />
-					</div>
-				</div>
-				<div className="col-sm-12 form-group">
-					<label className="col-sm-3">Scanning Window Size (bp)</label>
-					<div className="col-sm-4">
-						<input className="form-control" defaultValue={this.state.scanning_window_size} />
-					</div>
-				</div>
-				<div className="col-sm-12">
-					<textarea className="form-control" rows="10" onChange={(e) => this.codon_textarea_changed(e)} value={this.state.codon_input}></textarea>
-					<div className="form_group">
-						<label>Upload FASTA file</label>
-						<input type="file" onChange={ (e) => this.FASTA_file_changed(e) } />
-					</div>
+
+		var CpG_islands= this.detect_CpG_island();
+
+		return <div className="col-sm-12">
+			<div className="col-sm-12 form-group">
+				<label className="col-sm-3">Obs/Exp Threshold (0-1)</label>
+				<div className="col-sm-4">
+					<input className="form-control" onChange={(e) => this.obs_exp_threshold_changed(e)} defaultValue={this.state.obs_exp_threshold} />
 				</div>
 			</div>
+			<div className="col-sm-12 form-group">
+				<label className="col-sm-3">GC Content Threshold (0-1)</label>
+				<div className="col-sm-4">
+					<input className="form-control" onChange={(e) => this.gc_content_threshold_changed(e)}defaultValue={this.state.GC_content_threshold} />
+				</div>
+			</div>
+			<div className="col-sm-12 form-group">
+				<label className="col-sm-3">Scanning Window Size (bp)</label>
+				<div className="col-sm-4">
+					<input className="form-control" onChange={(e) => this.scanning_window_size_changed(e)} defaultValue={this.state.scanning_window_size} />
+				</div>
+			</div>
+			<div className="col-sm-12">
+				<textarea className="form-control" rows="10" onChange={(e) => this.base_textarea_changed(e)} value={this.state.base_input}></textarea>
+				<div className="form_group">
+					<label>Upload FASTA file</label>
+					<input type="file" onChange={ (e) => this.FASTA_file_changed(e) } />
+				</div>
+			</div>
+			<div className="col-sm-12">
+				<div>
+					<button type="button" className="btn btn-primary" data-toggle="collapse" data-target="#CpG_island_list_div">Fold CpG Island List</button>
+				</div>
+				{this.render_CpG_islands(CpG_islands)}
+			</div>
+		</div>
 	}
 }
 
--- biocheat/static/script/CpG_island/CpG_island.js
+++ biocheat/static/script/CpG_island/CpG_island.js
@@ -18,7 +18,8 @@ requirejs([], function () {
 			var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(CpGIsland).call(this, props));
 
 			_this.state = {
-				codon_input: "",
+				base_input: "",
+				base_seq: [],
 				obs_exp_threshold: 0.6,
 				GC_content_threshold: 0.5,
 				scanning_window_size: 200
@@ -27,11 +28,13 @@ requirejs([], function () {
 		}
 
 		_createClass(CpGIsland, [{
-			key: "codon_textarea_changed",
-			value: function codon_textarea_changed(e) {
+			key: "base_textarea_changed",
+			value: function base_textarea_changed(e) {
+				var base_seq = e.target.value.toUpperCase().replace(/[^ATUCG]/g, "").trim().split("");
+
 				this.setState({
-					codon_input: e.target.value,
-					codon_seq: e.target.value.toUpperCase().replace(/[^ATUCG]/g, "").trim()
+					base_input: e.target.value,
+					base_seq: base_seq
 
 				});
 			}
@@ -39,18 +42,130 @@ requirejs([], function () {
 			key: "FASTA_file_changed",
 			value: function FASTA_file_changed(e) {
 				var reader = new FileReader();
+				var base_seq = e.target.result.replace(/^>.+\n/, "").toUpperCase().replace(/[^ATUCG]/g, "").trim().split("");
+
 				reader.onload = function (e) {
 					this.setState({
-						codon_input: e.target.result.replace(/^>.+\n/, ""),
-						codon_seq: e.target.result.replace(/^>.+\n/, "").toUpperCase().replace(/[^ATUCG]/g, "").trim()
+						base_input: e.target.result.replace(/^>.+\n/, ""),
+						base_seq: base_seq
 					});
 				}.bind(this);
 				reader.readAsText(e.target.files[0]);
 			}
 		}, {
+			key: "detect_CpG_island",
+			value: function detect_CpG_island() {
+				var _this2 = this;
+
+				var num_c = 0;
+				var num_g = 0;
+				var num_cg = 0;
+				var scanning_window = [];
+
+				var CpG_islands = [];
+
+				this.state.base_seq.forEach(function (base, idx) {
+					var prev_input = scanning_window.slice(-1);
+					scanning_window.push(base);
+
+					if (base == "C") {
+						num_c++;
+					} else if (base == "G") {
+						num_g++;
+					}
+					if (prev_input == "C" && base == "G") {
+						num_cg++;
+					}
+
+					if (scanning_window.length < _this2.state.scanning_window_size) {
+						return;
+					}
+
+					if (scanning_window.length > _this2.state.scanning_window_size) {
+						var base_to_loss = scanning_window.shift();
+						if (base_to_loss == "C") {
+							num_c--;
+						}
+						if (base_to_loss == "G") {
+							num_g--;
+						}
+						if (base_to_loss == "C" && scanning_window[0] == "G") {
+							num_cg--;
+						}
+					}
+
+					var obs_exp = num_cg / (num_c * num_g / scanning_window.length);
+					var GC_content = (num_c + num_g) / scanning_window.length;
+
+					if (obs_exp > _this2.state.obs_exp_threshold && GC_content > _this2.state.GC_content_threshold) {
+						CpG_islands.push([idx, obs_exp, GC_content]);
+					}
+				});
+				return CpG_islands;
+			}
+		}, {
+			key: "obs_exp_threshold_changed",
+			value: function obs_exp_threshold_changed(e) {
+				this.setState({
+					obs_exp_threshold: e.target.value
+				});
+			}
+		}, {
+			key: "gc_content_threshold_changed",
+			value: function gc_content_threshold_changed(e) {
+				this.setState({
+					gc_content_threshold: e.target.value
+				});
+			}
+		}, {
+			key: "scanning_window_size_changed",
+			value: function scanning_window_size_changed(e) {
+				this.setState({
+					scanngin_window_size: e.target.value
+				});
+			}
+		}, {
+			key: "render_CpG_islands",
+			value: function render_CpG_islands(CpG_islands) {
+				var scanning_window_size = this.state.scanning_window_size;
+				return React.createElement(
+					"div",
+					{ id: "CpG_island_list_div", className: "collapse in" },
+					React.createElement(
+						"ul",
+						{ className: "list-unstyled" },
+						CpG_islands.map(function (e) {
+							return React.createElement(
+								"li",
+								null,
+								"CpG island at ",
+								React.createElement(
+									"b",
+									null,
+									e[0] - scanning_window_size
+								),
+								" to ",
+								React.createElement(
+									"b",
+									null,
+									e[0]
+								),
+								" (Obs/Exp=",
+								e[1],
+								", GC Content=",
+								e[2],
+								")"
+							);
+						})
+					)
+				);
+			}
+		}, {
 			key: "render",
 			value: function render() {
-				var _this2 = this;
+				var _this3 = this;
+
+				var CpG_islands = this.detect_CpG_island();
 
 				return React.createElement(
 					"div",
@@ -66,7 +181,9 @@ requirejs([], function () {
 						React.createElement(
 							"div",
 							{ className: "col-sm-4" },
-							React.createElement("input", { className: "form-control", defaultValue: this.state.obs_exp_threshold })
+							React.createElement("input", { className: "form-control", onChange: function onChange(e) {
+									return _this3.obs_exp_threshold_changed(e);
+								}, defaultValue: this.state.obs_exp_threshold })
 						)
 					),
 					React.createElement(
@@ -80,7 +197,9 @@ requirejs([], function () {
 						React.createElement(
 							"div",
 							{ className: "col-sm-4" },
-							React.createElement("input", { className: "form-control", defaultValue: this.state.GC_content_threshold })
+							React.createElement("input", { className: "form-control", onChange: function onChange(e) {
+									return _this3.gc_content_threshold_changed(e);
+								}, defaultValue: this.state.GC_content_threshold })
 						)
 					),
 					React.createElement(
@@ -94,15 +213,17 @@ requirejs([], function () {
 						React.createElement(
 							"div",
 							{ className: "col-sm-4" },
-							React.createElement("input", { className: "form-control", defaultValue: this.state.scanning_window_size })
+							React.createElement("input", { className: "form-control", onChange: function onChange(e) {
+									return _this3.scanning_window_size_changed(e);
+								}, defaultValue: this.state.scanning_window_size })
 						)
 					),
 					React.createElement(
 						"div",
 						{ className: "col-sm-12" },
 						React.createElement("textarea", { className: "form-control", rows: "10", onChange: function onChange(e) {
-								return _this2.codon_textarea_changed(e);
-							}, value: this.state.codon_input }),
+								return _this3.base_textarea_changed(e);
+							}, value: this.state.base_input }),
 						React.createElement(
 							"div",
 							{ className: "form_group" },
@@ -112,9 +233,23 @@ requirejs([], function () {
 								"Upload FASTA file"
 							),
 							React.createElement("input", { type: "file", onChange: function onChange(e) {
-									return _this2.FASTA_file_changed(e);
+									return _this3.FASTA_file_changed(e);
 								} })
 						)
+					),
+					React.createElement(
+						"div",
+						{ className: "col-sm-12" },
+						React.createElement(
+							"div",
+							null,
+							React.createElement(
+								"button",
+								{ type: "button", className: "btn btn-primary", "data-toggle": "collapse", "data-target": "#CpG_island_list_div" },
+								"Fold CpG Island List"
+							)
+						),
+						this.render_CpG_islands(CpG_islands)
 					)
 				);
 			}
