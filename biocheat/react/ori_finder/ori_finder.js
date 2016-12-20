b8bef5a9f15304b85564eb98b370072d6fb6c9f1
Ryan Rafe Lee <holo@wisewolf.org>
2016-12-20 17:19:31 +0900

    added gc skew graph

--- biocheat/react/ori_finder/ori_finder.js
+++ biocheat/react/ori_finder/ori_finder.js
@@ -1,9 +1,91 @@
 requirejs([], function(){
 
 class OriFinder extends React.Component{
-	render(){
+	constructor(props){
+		super(props);
+		this.state={
+			base_input: "",
+			base_seq: [],
+		}
+	}
 
+	base_textarea_changed(e){
+		var base_seq= e.target.value.toUpperCase().replace(/[^ATUCG]/g,"").trim().split("");
+
+		this.setState({
+			base_input: e.target.value,
+			base_seq: base_seq,
+
+		});
+	}
+
+	FASTA_file_changed(e){
+		var reader= new FileReader();
+
+		reader.onload= function (e){
+			var base_input=e.target.result.replace(/^>.+\n/,"");
+			var base_seq= base_input.toUpperCase().replace(/[^ATUCG]/g,"").trim().split("");
+			this.setState({
+				base_input: base_input,
+				base_seq: base_seq,
+			})
+
+		}.bind(this);
+		reader.readAsText(e.target.files[0]);
+	}
+
+	calc_gc_skew(base_seq){
+		var gc_skew=[0];
+		for(let base of base_seq){
+			switch(base){
+				case "G":
+					gc_skew.push(gc_skew.slice(-1)[0]+1);
+					break;
+				case "C":
+					gc_skew.push(gc_skew.slice(-1)[0]-1);
+					break;
+				default:
+					gc_skew.push(gc_skew.slice(-1)[0]);
+					break;
+			}
+		}
+		return gc_skew;
+	}
+
+	draw_skew_graph(gc_skew){
+		var height= 500;
+		var width= 720;
+		var padding= 30;
+
+		var xScale= d3.scaleLinear().domain([0, gc_skew.length]).range([padding, width-padding]);
+		var yScale= d3.scaleLinear().domain([d3.max(gc_skew), d3.min(gc_skew)]).range([padding, height-padding]);
+
+		var line_data=[];
+		for(let idx in gc_skew){
+			line_data.push({pos:xScale(idx), skew:yScale(gc_skew[idx])});
+		}
+
+		var valueline= d3.line().x((e)=>e.pos).y((e)=>e.skew);
+		var path_d= valueline(line_data);
+
+		return <svg height={height} width={width}>
+			<path d={path_d} stroke="black" strokeWidth={2} fill="none"></path>
+		</svg>
+	}
+
+	render(){
+		var gc_skew= this.calc_gc_skew(this.state.base_seq);
 		return <div className="col-sm-12">
+			<div className="col-sm-12">
+				<textarea className="form-control" rows="10" onChange={(e) => this.base_textarea_changed(e)} value={this.state.base_input}></textarea>
+				<div className="form_group">
+					<label>Upload FASTA file</label>
+					<input type="file" onChange={ (e) => this.FASTA_file_changed(e) } />
+				</div>
+			</div>
+			<div className="col-sm-12">
+				{this.draw_skew_graph(gc_skew)}
+			</div>
 		</div>
 	}
 }
@@ -11,6 +93,6 @@ class OriFinder extends React.Component{
 const mountingPoint= document.createElement('div');
 mountingPoint.className= 'react-app';
 document.getElementById("div_application").appendChild(mountingPoint);
-ReactDOM.render(<CpGIsland/>, mountingPoint);
+ReactDOM.render(<OriFinder/>, mountingPoint);
 
 });
--- biocheat/static/script/ori_finder/ori_finder.js
+++ biocheat/static/script/ori_finder/ori_finder.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -12,17 +12,147 @@ requirejs([], function () {
 	var OriFinder = function (_React$Component) {
 		_inherits(OriFinder, _React$Component);
 
-		function OriFinder() {
+		function OriFinder(props) {
 			_classCallCheck(this, OriFinder);
 
-			return _possibleConstructorReturn(this, Object.getPrototypeOf(OriFinder).apply(this, arguments));
+			var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(OriFinder).call(this, props));
+
+			_this.state = {
+				base_input: "",
+				base_seq: []
+			};
+			return _this;
 		}
 
 		_createClass(OriFinder, [{
-			key: 'render',
-			value: function render() {
+			key: "base_textarea_changed",
+			value: function base_textarea_changed(e) {
+				var base_seq = e.target.value.toUpperCase().replace(/[^ATUCG]/g, "").trim().split("");
 
-				return React.createElement('div', { className: 'col-sm-12' });
+				this.setState({
+					base_input: e.target.value,
+					base_seq: base_seq
+
+				});
+			}
+		}, {
+			key: "FASTA_file_changed",
+			value: function FASTA_file_changed(e) {
+				var reader = new FileReader();
+
+				reader.onload = function (e) {
+					var base_input = e.target.result.replace(/^>.+\n/, "");
+					var base_seq = base_input.toUpperCase().replace(/[^ATUCG]/g, "").trim().split("");
+					this.setState({
+						base_input: base_input,
+						base_seq: base_seq
+					});
+				}.bind(this);
+				reader.readAsText(e.target.files[0]);
+			}
+		}, {
+			key: "calc_gc_skew",
+			value: function calc_gc_skew(base_seq) {
+				var gc_skew = [0];
+				var _iteratorNormalCompletion = true;
+				var _didIteratorError = false;
+				var _iteratorError = undefined;
+
+				try {
+					for (var _iterator = base_seq[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+						var base = _step.value;
+
+						switch (base) {
+							case "G":
+								gc_skew.push(gc_skew.slice(-1)[0] + 1);
+								break;
+							case "C":
+								gc_skew.push(gc_skew.slice(-1)[0] - 1);
+								break;
+							default:
+								gc_skew.push(gc_skew.slice(-1)[0]);
+								break;
+						}
+					}
+				} catch (err) {
+					_didIteratorError = true;
+					_iteratorError = err;
+				} finally {
+					try {
+						if (!_iteratorNormalCompletion && _iterator.return) {
+							_iterator.return();
+						}
+					} finally {
+						if (_didIteratorError) {
+							throw _iteratorError;
+						}
+					}
+				}
+
+				return gc_skew;
+			}
+		}, {
+			key: "draw_skew_graph",
+			value: function draw_skew_graph(gc_skew) {
+				var height = 500;
+				var width = 720;
+				var padding = 30;
+
+				var xScale = d3.scaleLinear().domain([0, gc_skew.length]).range([padding, width - padding]);
+				var yScale = d3.scaleLinear().domain([d3.max(gc_skew), d3.min(gc_skew)]).range([padding, height - padding]);
+
+				var line_data = [];
+				for (var idx in gc_skew) {
+					line_data.push({ pos: xScale(idx), skew: yScale(gc_skew[idx]) });
+				}
+
+				var valueline = d3.line().x(function (e) {
+					return e.pos;
+				}).y(function (e) {
+					return e.skew;
+				});
+				var path_d = valueline(line_data);
+
+				return React.createElement(
+					"svg",
+					{ height: height, width: width },
+					React.createElement("path", { d: path_d, stroke: "black", strokeWidth: 2, fill: "none" })
+				);
+			}
+		}, {
+			key: "render",
+			value: function render() {
+				var _this2 = this;
+
+				var gc_skew = this.calc_gc_skew(this.state.base_seq);
+				return React.createElement(
+					"div",
+					{ className: "col-sm-12" },
+					React.createElement(
+						"div",
+						{ className: "col-sm-12" },
+						React.createElement("textarea", { className: "form-control", rows: "10", onChange: function onChange(e) {
+								return _this2.base_textarea_changed(e);
+							}, value: this.state.base_input }),
+						React.createElement(
+							"div",
+							{ className: "form_group" },
+							React.createElement(
+								"label",
+								null,
+								"Upload FASTA file"
+							),
+							React.createElement("input", { type: "file", onChange: function onChange(e) {
+									return _this2.FASTA_file_changed(e);
+								} })
+						)
+					),
+					React.createElement(
+						"div",
+						{ className: "col-sm-12" },
+						this.draw_skew_graph(gc_skew)
+					)
+				);
 			}
 		}]);
 
@@ -32,5 +162,5 @@ requirejs([], function () {
 	var mountingPoint = document.createElement('div');
 	mountingPoint.className = 'react-app';
 	document.getElementById("div_application").appendChild(mountingPoint);
-	ReactDOM.render(React.createElement(CpGIsland, null), mountingPoint);
+	ReactDOM.render(React.createElement(OriFinder, null), mountingPoint);
 });
