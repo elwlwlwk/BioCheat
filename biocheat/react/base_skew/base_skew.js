d9eb02399d2fc5d9d5c6d6b7934171ac395a6270
Ryan Rafe Lee <holo@wisewolf.org>
2017-01-31 04:45:39 +0900

    fixed bug

--- biocheat/react/base_skew/base_skew.js
+++ biocheat/react/base_skew/base_skew.js
@@ -26,7 +26,7 @@ class BaseSkew extends React.Component{
 		var reader= new FileReader();
 
 		reader.onload= function (e){
-			var base_input=e.target.result.replace(/^>.+\n/,"");
+			var base_input=e.target.result.replace(/^>.+/,"");
 			var base_seq= base_input.toUpperCase().replace(/[^ATUCG]/g,"").trim().split("");
 			this.setState({
 				base_input: base_input,
